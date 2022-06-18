41ded7a8697931020427f75037f29c7f3a51d334
jim2832 <jim880705@gmail.com>
2022-06-19 01:50:04 +0800

    update

--- practice/feof/feof.c
+++ practice/feof/feof.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
 	FILE *file;
-	file = fopen("abc.txt", "r");
-	char str[128];
+	char file_name[128];
+	scanf("%s", file_name); //input file name
+	file = fopen(file_name, "r"); //load the file
 	char ch;
 
 	while(1){
-		fscanf(file, "%c", &ch);
-		putchar(ch);
-		if(feof(file)){
+		fscanf(file, "%c", &ch);//use char can copy the whole file
+		
+		if(feof(file)){ //detect the end of file
 			break;
 		}
+
+		putchar(ch);
+		
 	}
 
 	fclose(file);
