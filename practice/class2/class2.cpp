8292f90e35bfdae6acac2df24386074eadc4147e
jim2832 <jim880705@gmail.com>
2022-06-22 00:15:06 +0800

    update

--- practice/class2/class2.cpp
+++ practice/class2/class2.cpp
@@ -10,9 +10,44 @@ class Pokemon{
             cout << "HP: " << HpCur << "/" << HpMax << endl;
         }
 
-        void Attack(Pokemon &target);
-        void Defense(int n);
-        void Cure();
+        void SetData(string na, int lv, int hpcur, int hpmax){
+            if(hpcur > hpmax){
+                cout << "HpCur error" << endl;
+                return;
+            }
+            name = na;
+            level = lv;
+            HpCur = hpcur;
+            HpMax = hpmax;
+        }
+
+        void Attack(Pokemon &target){
+            if(HpCur <= 0){
+                cout << name << " can't attatck" << endl;
+                return;
+            }
+            if(target.HpCur <= 0){
+                cout << "The target " << target.name
+                << "can't be attatcked" << endl;
+                return;
+            }
+            cout << name << " attatcked " 
+            << target.name << " for " << level << " points " << endl;
+            target.Defense(level);
+
+        }
+
+        void Defense(int n){
+            HpCur -= n;
+            if(HpCur <= 0){
+                cout << name << " lose his power" << endl;
+            }
+        }
+
+        void Cure(){
+            cout << name << "restore health totally" << endl;
+            HpCur = HpMax;
+        }
     
     private:
         string name;
@@ -23,7 +58,14 @@ class Pokemon{
 
 int main(void){
     Pokemon p1, p2;
+    p1.SetData("Pika", 10, 15, 15);
+    p2.SetData("Dragon", 15, 25, 25);
 
+    p1.Attack(p2);
+    p2.Attack(p1);
+
+    p1.Show();
+    p2.Show();
 
     return 0;
 }
--- practice/class_prac/class_prac.cpp
+++ practice/class_prac/class_prac.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 class Square{
     public:
-        int len;
-
+        //return the area
         int area(){
-            int area = len * len;
+            int area = pow(len, 2);
             return area;
         }
+
+        //set the length
+        void setLen(int n){
+            if(n < 0){
+                cout << "The length can't be less than zero" << endl;
+                len = 0;
+                return;
+            }
+            len = n;
+        }
+
+        //return the length
+        int getLen(){
+            return len;
+        }
+
+    private:
+        int len;
 };
 
 int main(void){
     Square S1, S2;
-    S1.len = 10;
-    S2.len = 20;
-    cout << S1.area() << endl;
-    cout << S2.area() << endl;
+    S1.setLen(10);
+    S2.setLen(20);
+    
+    cout << "The length of S1 is: " << S1.getLen() << ", "
+    << "The area of S1 is: " << S1.area() << endl;
+
+    cout << "The length of S2 is: " << S2.getLen() << ", "
+    << "The area of S2 is: " << S2.area() << endl;
 
     return 0;
 }
