ff266573bb39a25f99f9f4552842dcbe10bfd998
jim2832 <jim880705@gmail.com>
2022-06-22 02:29:13 +0800

    ok

---
+++ HW/HW8/PokemonIV.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Pokemon{
+    char Name[100];
+    int Lv;
+    int Hp;
+};
+
+void InputData(struct Pokemon *); // call by address
+void ShowInfo(struct Pokemon);
+
+int main(void){
+    struct Pokemon p1, p2;
+
+    InputData(&p1);
+    InputData(&p2);
+
+    ShowInfo(p1);
+    puts("");
+    ShowInfo(p2);
+
+    return 0;
+}
+
+void InputData(struct Pokemon *p){
+    scanf("%s", p -> Name);
+    scanf("%d", &(*p).Lv);
+    scanf("%d", &(*p).Hp);
+}
+
+void ShowInfo(struct Pokemon p){
+    printf("Name: %s\n", p.Name);
+    printf("Name: %d\n", p.Lv);
+    printf("Name: %d\n", p.Hp);
+}
--- practice/overload2/overload2.cpp
+++ practice/overload2/overload2.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void show(struct Person person);
+void show(struct Person x);
 
 struct Person{
     string name;
@@ -21,4 +22,10 @@ void show(struct Person &person){ //call by reference
     cout << "Name: " << person.name << endl;
     cout << "Height: " << person.height << endl;
     cout << "Weight: " << person.weight << endl;
+}
+
+void show(struct Person x){
+    cout << "Name: " << x.name << endl;
+    cout << "Height: " << x.height << endl;
+    cout << "Weight: " << x.weight << endl;
 }
